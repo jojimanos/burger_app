ba37fad82e5ad0837b9dfa441f417b47b13ca600
jojimanos <manosgeorge7@gmail.com>
2022-11-21 15:24:17 +0200

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,32 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [lettuce, setLettuce] = useState(0)
+  const [tomato, setTomato] = useState(0)
+  const [beef, setBeef] = useState(0)
+  const [bacon, setBacon] = useState(0)
+  const [cheese, setCheese] = useState(0)
+
+  function add(ingredient: number, setIngredient: Function) { setIngredient(ingredient + 1) }
+  function remove(ingredient: number, setIngredient: Function) { setIngredient(ingredient - 1) }
+
+  //for (i = 0, )
+  const burgerContents: any = () => {
+
+    let burger: any = []
+
+    for (let i = 0; i < lettuce; i++) {
+      burger.push(<div key={burger.length} className='lettuce'>Lettuce</div>)
+      console.log(burger)
+    }
+    return burger
+    
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,60 +36,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>{burgerContents()}</div>
+        <div>
+          <p>Lettuce</p>
+          <button onClick={() => { add(lettuce, setLettuce) }}>I want more</button>
+          <button onClick={() => { remove(lettuce, setLettuce) }}>I want less</button>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
